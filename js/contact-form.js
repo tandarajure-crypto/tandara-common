4ae98640bc7616a617714ee9f1d4d5b598d49d84
JURE TANDARA <287090835+tandarajure-crypto@users.noreply.github.com>
2026-09-17 05:34:18 -0700

    Update contact-form.js

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,1 +1,18 @@
 
+(() => {
+"use strict";
+
+const params =
+new URLSearchParams(window.location.search);
+
+const success =
+document.getElementById("form-success");
+
+if (
+success &&
+params.get("sent") === "1"
+) {
+success.hidden = false;
+}
+
+})();
